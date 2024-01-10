2db608b5ce2e9cb9257876c1fa2f0b7772b7afd1
RavenAV <anubis.27007@mail.ru>
2024-01-10 21:15:50 +0300

    Creating DbContext, generating database migrations, configuring entities for db and applying a database migration on startup

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using GameStore.Api.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,8 +8,13 @@ public class GameStoreContext : DbContext
 {
     public GameStoreContext(DbContextOptions<GameStoreContext> options)
         : base(options)
-    { 
+    {
     }
 
     public DbSet<Game> Games => Set<Game>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
